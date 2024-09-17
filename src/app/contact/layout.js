c53f997bb400b4c8bcd23beed6ccd0698ddd3f86
Talha-TN <nawazt964@gmail.com>
2024-09-17 12:31:22 -0700

    implement conditional rendering of layout.js according to current path name

--- src/app/contact/layout.js
+++ src/app/contact/layout.js
@@ -1,9 +1,14 @@
+"use client"
 import React from 'react'
-
+import { usePathname } from 'next/navigation'
 const layout = ({children}) => {
+    const pathName = usePathname()
+    console.log(pathName);
+    
   return (
     <>
-    <h1>comman layout for contact page</h1>
+    {/* render layout in children according to condition which we get from current pathname */}
+    {pathName !=="/contact/company"?<h1>comman layout for contact page</h1>:null}
     {children}
     </>
     
