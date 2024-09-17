3e979d951df2fc61b99d162c7c1a9fcd7b773c8e
Talha-TN <nawazt964@gmail.com>
2024-09-17 11:04:02 -0700

    implement routing using Link and navigation routing using useRouter

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,28 @@
 "use client";
 import EventFunctionState from "./components/EventFunctionState";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter()
+  const handleNavigation=(routerName)=>{
+    router.push(routerName)
+  }
+  const buttonStyles="ml-4 font-serif text-2xl text-red-600 hover:text-white"
   return (
     <div className="text-4xl text-center mt-10">
-       <h1>hello world</h1>
+      {/* link bas routing */}
+      {/* <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/home">Home</Link>
+      <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/about">About</Link>
+      <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/contact">Contact</Link>
+      */}
+      {/* navigaton base routing */}
+      {/* <button onClick={()=>router.push("/home")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">Home</button>
+      <button onClick={()=>router.push("/about")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">About</button>
+      <button onClick={()=>router.push("/contact")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">Contact</button> */}
+      {/* make function for handle routing and make variable for styling to avoid code dulplication */}
+      <button onClick={()=>handleNavigation("/home")} className={buttonStyles}>Home</button>
+      <button onClick={()=>handleNavigation("/about")} className={buttonStyles}>About</button>
+      <button onClick={()=>handleNavigation("/contact")} className={buttonStyles}>Contact</button>
        <EventFunctionState/>
     </div>
   );
