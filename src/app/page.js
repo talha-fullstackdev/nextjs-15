63004bc08072d317dd75fa4c9f6026e27e86a8b7
Talha-TN <nawazt964@gmail.com>
2024-09-17 11:07:38 -0700

    make adjustment

--- src/app/page.js
+++ src/app/page.js
@@ -10,15 +10,15 @@ export default function Home() {
   const buttonStyles="ml-4 font-serif text-2xl text-red-600 hover:text-white"
   return (
     <div className="text-4xl text-center mt-10">
-      {/* link bas routing */}
-      {/* <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/home">Home</Link>
-      <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/about">About</Link>
-      <Link className="ml-4 font-serif text-2xl text-red-600 hover:text-white" href="/contact">Contact</Link>
-      */}
+      {/* link base routing */}
+      {/* <Link className={buttonStyles} href="/home">Home</Link>
+      <Link className={buttonStyles} href="/about">About</Link>
+      <Link className={buttonStyles} href="/contact">Contact</Link> */}
+     
       {/* navigaton base routing */}
-      {/* <button onClick={()=>router.push("/home")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">Home</button>
-      <button onClick={()=>router.push("/about")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">About</button>
-      <button onClick={()=>router.push("/contact")} className="ml-4 font-serif text-2xl text-red-600 hover:text-white">Contact</button> */}
+      {/* <button onClick={()=>router.push("/home")} className={buttonStyles}>Home</button>
+      <button onClick={()=>router.push("/about")} className={buttonStyles}>About</button>
+      <button onClick={()=>router.push("/contact")} className={buttonStyles}>Contact</button> */}
       {/* make function for handle routing and make variable for styling to avoid code dulplication */}
       <button onClick={()=>handleNavigation("/home")} className={buttonStyles}>Home</button>
       <button onClick={()=>handleNavigation("/about")} className={buttonStyles}>About</button>
