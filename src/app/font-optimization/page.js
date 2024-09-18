9f26ac4862d9c5f7ccd7723709b7e24d09e003e7
Talha-TN <nawazt964@gmail.com>
2024-09-18 13:18:31 -0700

    implement image optimization and font optimization

---
+++ src/app/font-optimization/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FontOptimization = () => {
+    const fontStyle='text-2xl font-semibold mt-4 text-center'
+  return (
+    <div>
+        <h2 className={fontStyle}>Font Optimization in Next.JS</h2>
+        <h2 className={fontStyle}>hello</h2>
+        <h2 className={fontStyle}>welcome</h2>
+    </div>
+  )
+}
+
+export default FontOptimization
---
+++ src/app/image-optimization/page.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import img from "../favicon.ico";
+const ImageOptimization = () => {
+  console.log(img);
+
+  return (
+    <>
+      <div>image optimization in next.js</div>
+      {/* <Image src={img} width={100} height={80} alt='logo-image'/> */}
+      <Image
+        src={
+          "https://images.pexels.com/photos/28410262/pexels-photo-28410262/free-photo-of-default.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        }
+        alt="pixel image"
+        width={400}
+        height={350}
+      />
+    </>
+  );
+};
+
+export default ImageOptimization;
