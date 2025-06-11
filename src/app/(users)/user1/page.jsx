a64d2929f74d8ad31094b1387542e33fef5ca107
talha-fullstackdev <talhafullstack786@gmail.com>
2025-06-11 14:22:20 +0500

    learn about route group (users) made this route group

---
+++ src/app/(users)/user1/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User1  = () => {
+  return (
+    <div>User1 page </div>
+  )
+}
+
+export default User1
---
+++ src/app/(users)/user2/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User2  = () => {
+  return (
+    <div>User2 page </div>
+  )
+}
+
+export default User2 
